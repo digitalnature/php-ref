fada32b5e45d46069481e7374a3676692a39cd08
digitalnature <hello@digitalnature.eu>
2012-12-28 15:13:48 +0200

    optimized js and dropped support for IE 8 and lower

--- ref.js
+++ ref.js
@@ -1,81 +1,43 @@
+window.addEventListener('load', function(){
 
-var tooltip = { name: 'rTip', offsetX: 0, offsetY: 15, tip: null };
+  this.rTip = { e: document.getElementById('rTip') };
 
-tooltip.init = function () {
-  
-  var tipContainer = document.getElementById(tooltip.name);
-
-  if(!tipContainer){
-    tipContainer = document.createElement('div');
-    tipContainer.setAttribute('id', tooltip.name);          
-    tipContainer.className = 'reflekt';
-    document.getElementsByTagName('body').item(0).appendChild(tipContainer);
+  if(!this.rTip.e){
+    this.rTip.e = document.createElement('div');
+    this.rTip.e.id = 'rTip';
+    document.getElementsByTagName('body').item(0).appendChild(this.rTip.e);
   }
-  
-  this.tip = document.getElementById(this.name);
 
-  if(this.tip)
-    document.onmousemove = function(e){ tooltip.move(e); };
-
-  var nfoTags = document.getElementsByTagName('code');
-  if(nfoTags){
-    for(var i = 0; i < nfoTags.length; i ++){      
-      var title = nfoTags[i].innerHTML;
-      if(title){
-        nfoTags[i].parentNode.setAttribute('tip', title);              
-        nfoTags[i].parentNode.onmouseover = function(){ tooltip.show(this.getAttribute('tip')); };
-        nfoTags[i].parentNode.onmouseout = function(){ tooltip.hide(); };
-      }
-    }
-  }
-  
-};
-
-tooltip.move = function(evt){
-  var x = 0, y = 0;
-  if(document.all){
-    x = (document.documentElement && document.documentElement.scrollLeft) ? document.documentElement.scrollLeft : document.body.scrollLeft;
-    y = (document.documentElement && document.documentElement.scrollTop) ? document.documentElement.scrollTop : document.body.scrollTop;
-    x += window.event.clientX;
-    y += window.event.clientY;
-    
-  }else{
-    x = evt.pageX;
-    y = evt.pageY;
+  document.onmousemove = function(evt){ 
+    rTip.e.style.left = evt.pageX + 'px';
+    rTip.e.style.top = evt.pageY + 'px';
   };
-  this.tip.style.left = (x + this.offsetX) + 'px';
-  this.tip.style.top = (y + this.offsetY) + 'px';
-};
 
-tooltip.show = function(text){
-  if(!this.tip) return;
-  this.tip.innerHTML = text;
-  this.tip.style.display = 'block';
-};
+  var tags = document.getElementsByTagName('code');
 
-tooltip.hide = function(){
-  if(!this.tip) return;
-  this.tip.innerHTML = '';
-  this.tip.style.display = 'none';
-};
+  for(var i in tags){
 
+    if(!tags[i].innerHTML)
+      continue;
 
+    tags[i].parentNode.setAttribute('txt', tags[i].innerHTML);
+    tags[i].parentNode.onmouseover = function(){ 
+      if(!rTip.e) return;
+      rTip.e.innerHTML = this.getAttribute('txt');
+      rTip.e.style.display = 'block';
+    };
 
-var addEvent = function(evt, elem, func){      
-  if(elem.addEventListener){
-    elem.addEventListener(evt, func, false);
-    return;
-  }  
+    tags[i].parentNode.onmouseout = function(){ 
+      if(!rTip.e) return;          
+      rTip.e.innerHTML = '';
+      rTip.e.style.display = 'none';
+    };
   
-  elem.attachEvent('on' + evt, func);          
-};      
-
-addEvent('load', window, function(e){
-  tooltip.init();
+  }
 });      
 
-addEvent('click', document, function(e){
-  var target = e.target || e.srcElement;
+document.addEventListener('click', function(evt){
+  var target = evt.target || evt.srcElement;
   if(target.className.indexOf('rToggle') !== -1)
-    target.className = target.className.replace(/\bexp\b|\bcol\b/, function(m){ return m == 'col' ? 'exp' : 'col'; });
+    target.className = target.className.replace(/\bexp\b|\bcol\b/, function(m){ return m !== 'col' ? 'col' : 'exp'; });
 });
