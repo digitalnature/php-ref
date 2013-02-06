efc80e92e0006fab4ec2f5fa94d339007a3acb8a
digitalnature <hello@digitalnature.eu>
2013-02-06 17:25:54 +0200

    js improvements + tooltip fade fx

--- ref.js
+++ ref.js
@@ -1,33 +1,36 @@
-
 window.addEventListener('load', function(){
 
-  this.rTip = {};
-  this.rTip.e = document.createElement('div');
-  this.rTip.e.id = 'rTip';
+  var tip = document.createElement('div'),      
+      refs = document.getElementsByClassName('ref'),
+      nfos = document.getElementsByClassName('rHasTip');     
 
-  var ref = document.getElementsByClassName('ref')[0],
-      tags = document.getElementsByClassName('rHasTip');
+  for(var i = 0, max = refs.length; i < max; i++)
+    document.onmousemove = function(e){
+      if(tip.className.indexOf('visible') < 0)
+        return;
+      tip.style.top = ((document.documentElement.clientHeight - e.clientY) < tip.offsetHeight + 20 ? (e.pageY - tip.offsetHeight) : e.pageY) + 'px';
+      tip.style.left = ((document.documentElement.clientWidth - e.clientX) < tip.offsetWidth + 20 ? (e.pageX - tip.offsetWidth) : e.pageX) + 'px';
+    };      
 
-  for(var i in tags){
+  for(var i = 0, max = nfos.length; i < max; i++){
 
-    tags[i].onmouseover = function(){ 
-      rTip.e.innerHTML = this.getElementsByTagName('q')[0].innerHTML;
-      rTip.e.className = 'ref visible';
+    nfos[i].onmouseover = function(){ 
+      tip.className += 'ref visible';      
+      tip.innerHTML = this.getElementsByTagName('q')[0].innerHTML;
+      window.clearTimeout(tip.fadeOut);
     };
 
-    tags[i].onmouseout = function(){        
-      rTip.e.innerHTML = '';
-      rTip.e.className = '';
+    nfos[i].onmouseout = function(){
+      tip.className = 'ref visible fadingOut';
+      tip.fadeOut = window.setTimeout(function(){
+        tip.innerHTML = '';
+        tip.className = '';
+      }, 250);
+    
     };
   
   }
 
-  document.onmousemove = function(evt){
-    if(rTip.e.className.indexOf('visible') < 0)
-      return;
-    rTip.e.style.top = ((document.documentElement.clientHeight - event.clientY) < rTip.e.offsetHeight + 20 ? (event.pageY - rTip.e.offsetHeight) : event.pageY) + 'px';
-    rTip.e.style.left = ((document.documentElement.clientWidth - event.clientX) < rTip.e.offsetWidth + 20 ? (event.pageX - rTip.e.offsetWidth) : event.pageX) + 'px';
-  };
-
-  ref.parentNode.insertBefore(this.rTip.e, ref.nextSibling);
-});      
+  tip.id = 'rTip';
+  document.body.appendChild(tip);
+});
