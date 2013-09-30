60180910d6a508b868f238a920489de971b6e32b
Joe Walker <jwalker.joe@gmail.com>
2013-09-30 16:52:21 +1000

    Update ref.js
    Changed expand key from 'x' to ESC

--- ref.js
+++ ref.js
@@ -47,7 +47,7 @@ window.addEventListener('load', function(){
 });
 
 window.addEventListener('keydown', function(e){
-  if(e.keyCode != 88)
+  if(e.keyCode != 27)
     return;
 
   var kbds = document.querySelectorAll('.ref [data-toggle]'),
